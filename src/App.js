a2ead6846cee469cb1d8c92b47c1ffe4ffd22497
Kiluk <lukasz96karpinski@gmail.com>
2024-01-15 16:44:28 +0100

    Frontpage changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NavBar from './NavBar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" text="Real Ninja">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to the Shinobi World
