76f94c14d271217cc9b095a109cd5f06d41ea7e0
Kiluk <lukasz96karpinski@gmail.com>
2025-03-02 10:29:35 +0100

    Login popoup change

--- frontend/src/LogInButton.js
+++ frontend/src/LogInButton.js
@@ -1,32 +1,17 @@
 import React, { useState } from "react";
 import "./Login.css"; // Stylizacja popupu
 
-function LogInButton() {
-  const [isOpen, setIsOpen] = useState(false);
-
+function LogPopup({ onClose }) {
   return (
-    <div>
-      {/* Przycisk otwierający popup */}
-      <button onClick={() => setIsOpen(true)} className="login-btn">
-        Log in
-      </button>
-
-      {/* Popup logowania */}
-      {isOpen && (
         <div className="popup">
           <div className="popup-inner">
             <h2>Zaloguj się</h2>
             <input type="text" placeholder="Login" className="input-field" />
             <input type="password" placeholder="Hasło" className="input-field" />
-            
             <button className="submit-btn">Zaloguj</button>
-            <button onClick={() => setIsOpen(false)} className="close-btn">
-              Zamknij
-            </button>
+            <button onClick={(onClose)}> Zamknij</button>
           </div>
         </div>
-      )}
-    </div>
   );
 }
 
--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <nav className="navbar">
       <Link to="/">Home</Link>
       <Link to="/register">Rejestracja</Link>
-      <Link to="/login">Login</Link>
+      <button onClick={() => setShowLogin(true)}>Login</button>   {showLogin && <LoginPopup onClose={() => setShowLogin(false)} />}
       <Link to="/charactercreation">Character Creation</Link>
     </nav>
   );
